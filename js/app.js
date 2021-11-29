8da278bdb03bc3d853689628faa8895c26fcf724
Thibault <thi95540@gmail.com>
2021-11-29 07:12:33 +0100

    CSS + JAVASCRIPT

--- js/app.js
+++ js/app.js
@@ -14,6 +14,13 @@ for (let i = 0; i < recipes.length; i++) {
     ustensils: recipes[i].ustensils,
   });
 }
+
+// log the new array
+console.log(recipesArray);
+
+
+
+
 // separete recipe by id
 const recipeById = {};
 for (let i = 0; i < recipesArray.length; i++) {
@@ -29,28 +36,123 @@ for (let i = 0; i < recipesArray.length; i++) {
     <div class="card">
     <img class="card-img-top" src="" alt="${recipe.name}">
     <div class="card-body">
+      <div class="card-body-top">
       <h5 class="card-title">${recipe.name}</h5>
+      <div class="recipe-item__info-item clock">
+      <i class="far fa-clock fa-lg"></i>
+      <h5>${recipe.time} min</h5>
+      </div>
+      </div>
       <div class="recipe-item__content">
-      <div class="recipe-item__info">
-                <div class="recipe-item__info-item">
-                    <i class="fas fa-utensils"></i>
-                    <span>${recipe.ustensils}</span>
+          <div class="recipe-item__info">
+                <div class="recipe-item__info-item ingredients">
                 </div>
-                <div class="recipe-item__info-item">
-                    <i class="fas fa-clock"></i>
-                    <span>${recipe.time}</span>
+                <div class="recipe-item__info-item description">
+                <p class="recipe-item__description">${recipe.description}</p>
                 </div>
-                <div class="recipe-item__info-item">
-                    <i class="fas fa-users"></i>
-                    <span>${recipe.servings}</span>
+                <div class="recipe-item__info-item ustensils">
+                <p>ustensils: ${recipe.ustensils}</p>
                 </div>
-            <p class="recipe-item__description">${recipe.description}</p>
-            
-            </div>
-        </div>
+          </div>
+       </div>
     </div>
   </div>
         
     `;
   recipeList.appendChild(recipeItem);
+
+  // ellipsis to the description
+  const description = recipeItem.querySelector(".recipe-item__description");
+  if (description.textContent.length > 100) {
+    description.textContent = description.textContent.substring(0, 200) + "...";
+  } else {
+    description.textContent = description.textContent;
+  }
+}
+
+
+
+// add ingredients to the DOM for each recipes (ingredient, quantity, unit)
+const ingredients = document.querySelectorAll(".recipe-item__info-item.ingredients");
+for (let i = 0; i < ingredients.length; i++) {
+  const ingredient = ingredients[i];
+  const recipe = recipesArray[i];
+  for (let j = 0; j < recipe.ingredients.length; j++) {
+    const recipeIngredient = recipe.ingredients[j];
+    // if unit and quantity is not defined, set it to " "
+    if (recipeIngredient.unit === undefined) {
+      recipeIngredient.unit = " ";
+    }
+    if (recipeIngredient.quantity === undefined) {
+      recipeIngredient.quantity = " ";
+    } else // add ":" before quantity
+    {
+      recipeIngredient.quantity = ": " + recipeIngredient.quantity;
+    }
+    ingredient.innerHTML += `
+      <div class="recipe-item__info-item-ingredient">
+      <span class="bold-ingredient">${recipeIngredient.ingredient}</span>
+      <span>${recipeIngredient.quantity}</span>
+      <span class="unit">${recipeIngredient.unit}</span>
+      </div>
+    `;
+
+    //add ingredient only 1 time to the first dropdown
+    if (j === 0) {
+      const dropdown = document.querySelector(".add-ingredients");
+      const option = document.createElement("li");
+      option.value = recipeIngredient.ingredient;
+      option.textContent = recipeIngredient.ingredient;
+      dropdown.appendChild(option);
+
+      // display none by default
+      option.style.display = "none";
+      // add class to the option
+      option.classList.add("dropdown-item");
+      // display the option when the button is clicked
+      dropdown.addEventListener("click", () => {
+        option.style.display = "block";
+      });
+
+      // when an ingredient is selected, display only the recipe that contains the ingredient
+      option.addEventListener("click", () => {
+        const recipesContainingIngredient = document.querySelectorAll(".recipe-item");
+        for (let k = 0; k < recipesContainingIngredient.length; k++) {
+          const recipeContainingIngredient = recipesContainingIngredient[k];
+          const ingredients = recipeContainingIngredient.querySelectorAll(".recipe-item__info-item-ingredient");
+          for (let l = 0; l < ingredients.length; l++) {
+            const ingredient = ingredients[l];
+            if (ingredient.textContent.includes(option.textContent)) {
+              recipeContainingIngredient.style.display = "block";
+            } else {
+              recipeContainingIngredient.style.display = "none";
+            }
+          }
+        }
+      });
+    }
+
+}
+
+// use text search to filter recipes by using everythings in the recipe
+const search = document.querySelector(".search");
+search.addEventListener("keyup", (e) => {
+  const searchText = e.target.value.toLowerCase();
+  const recipes = document.querySelectorAll(".recipe-item");
+  recipes.forEach((recipe) => {
+    const recipeName = recipe.querySelector(".card-title").textContent.toLowerCase();
+    const recipeDescription = recipe.querySelector(".description").textContent.toLowerCase();
+    const recipeIngredients = recipe.querySelector(".ingredients").textContent.toLowerCase();
+    const ustensils = recipe.querySelector(".ustensils").textContent.toLowerCase();
+    if (recipeName.includes(searchText) || recipeDescription.includes(searchText) || recipeIngredients.includes(searchText) || ustensils.includes(searchText)) {
+      recipe.style.display = "block";
+    } else {
+      recipe.style.display = "none";
+    }
+    // need 3 caracters to search
+    if (searchText.length < 3) {
+      recipe.style.display = "block";
+    }
+  });
+});
 }
